8bd07fadd3d0d0670bea3d2189f26dfe433a3ed9
RoySamson-stack <rsam9009@gmail.com>
2022-05-17 14:43:10 +0300

    fixing error

--- queston2.py
+++ queston2.py
@@ -5,7 +5,7 @@ def searchname(fn):
     name= []
     name_a = [element for element in open(fn, 'r') if element.startswith("A")]
     name.append(name_a)
-    print(mylist)
+    print(name)
 searchname("names.txt")
 
 # #user enter the first name
@@ -13,8 +13,8 @@ def searchname(fn):
     name= []
     name_letter = input("Enter a the first letter of the name: ")
     name_a = [element for element in open(fn, 'r') if element.startswith(name_letter)]
-    name.append(name_a)
-    print(mylist)
+    name.append(name_letter)
+    print(name)
 searchname("names.txt")
 
 #searching age of the person
@@ -22,30 +22,33 @@ def searchage(fn):
     age = []
     for line in open(fn, 'r'):
         if re.search(str(5), line):
-            words=line.split()
-            age.append(words)
+            ages=line.split()
+            age.append(ages)
     print(age)
 searchage("names.txt")   
 
-#searchingthe age of the person using user input 
+#searching the age of the person using user input 
 def searchage(fn):
     age = []
     age_enter = input("Enter the age:")
     for line in open(fn, 'r'):
         if re.search(str(age_enter), line):
-            words=line.split()
-            age.append(words)
+            ages=line.split()
+            age.append(ages)
     print(age)
 searchage("names.txt")   
 
 #main funtion
 def main():
     ans = input("Kindly choose 1 to seach name and 2 to search by age:")
-    if ans == "1":
+    if ans == 1:
         searchname("names.txt")
-    elif ans == "2":
+    elif ans == 2:
         searchname("names.txt") 
     else:
         print("You entered an invalid option")
         raise SystemExit
+    
+if __name__ == "__main__":
+    main()    
     
